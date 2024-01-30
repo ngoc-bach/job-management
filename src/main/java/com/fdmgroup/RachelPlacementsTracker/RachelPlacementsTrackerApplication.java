17e3fc872bc54e93b43e44b7f8b8a1c340f3e4c9
Rachel <thanhngocbach0904@gmail.com>
2024-01-30 11:54:20 +1100

    Initial commit

---
+++ src/main/java/com/fdmgroup/RachelPlacementsTracker/RachelPlacementsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.RachelPlacementsTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachelPlacementsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachelPlacementsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/RachelPlacementsTracker/RachelPlacementsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.RachelPlacementsTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RachelPlacementsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
